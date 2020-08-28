8dba5f73966d26b1b6ed02c2631c1c9aaa34b50b
Bogodar Shmyglyk <bogodar@gmail.com>
2020-08-28 23:02:15 +0300

    Added check list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'user-list';
+  typesOfShoes: string[] = ['New items','Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
